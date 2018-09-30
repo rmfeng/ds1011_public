369e7ba0bfd7c516bdf4f1d2c249fef1ed3e1624
Rong Feng <rmfeng@gmail.com>
2018-09-30 12:09:01 -0400

    first working training

--- hw1/IMDBDatum.py
+++ hw1/IMDBDatum.py
@@ -2,6 +2,10 @@
 # reference https://github.com/nyu-mll/DS-GA-1011-Fall2017
 """
 import os
+from tqdm import tqdm_notebook as tqdm
+import torch
+from torch.utils.data import Dataset
+import numpy as np
 
 
 class IMDBDatum:
@@ -30,6 +34,29 @@ class IMDBDatum:
 
     def set_tokens(self, tokens):
         self.tokens = tokens
+
+
+class IMDBDataset(Dataset):
+    """
+    Class that represents a train/validation/test dataset that's readable for PyTorch
+    Note that this class inherits torch.utils.data.Dataset
+    """
+
+    def __init__(self, data_list):
+        """
+        @param data_list: list of IMDBDatum
+        """
+        self.data_list = data_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, key):
+        """
+        Triggered when you call dataset[i]
+        """
+        token_idx, label = self.data_list[key].token_idx, self.data_list[key].label
+        return [token_idx, len(token_idx), label]
 
 
 def preprocess_text(text):
@@ -63,7 +90,28 @@ def construct_dataset(dataset_dir, dataset_size, offset=0):
     output = []
     all_pos = os.listdir(pos_dir)
     all_neg = os.listdir(neg_dir)
-    for i in range(offset, offset + single_label_size):
+    for i in tqdm(range(offset, offset + single_label_size)):
         output.append(read_file_as_datum(os.path.join(pos_dir, all_pos[i]), 1))
         output.append(read_file_as_datum(os.path.join(neg_dir, all_neg[i]), 0))
     return output
+
+
+def imdb_collate_func(batch):
+    """
+    Customized function for DataLoader that dynamically pads the batch so that all
+    data have the same length
+    """
+    data_list = []
+    label_list = []
+    length_list = []
+    for datum in batch:
+        label_list.append(datum[2])
+        length_list.append(datum[1])
+    max_length = np.max(length_list)
+    # padding
+    for datum in batch:
+        padded_vec = np.pad(np.array(datum[0]),
+                            pad_width=(0, max_length - datum[1]),
+                            mode="constant", constant_values=0)
+        data_list.append(padded_vec)
+    return [torch.from_numpy(np.array(data_list)), torch.LongTensor(length_list), torch.LongTensor(label_list)]
--- hw1/ngrams.py
+++ hw1/ngrams.py
@@ -1,8 +1,12 @@
+"""
+responsible for tokenizing and building dataset pipe
+"""
 import spacy
 import string
 from collections import Counter
 from sklearn.feature_extraction import stop_words
 from tqdm import tqdm_notebook as tqdm
+from tqdm import tnrange
 
 tokenizer = spacy.load('en_core_web_sm')
 punctuations = string.punctuation
@@ -11,38 +15,47 @@ PAD_TOKEN, PAD_IDX = '<pad>', 0
 UNK_TOKEN, UNK_IDX = '<unk>', 1
 
 
-def tokenize(sent, remove_stopwords=True, remove_punc=True):
+def tokenize(sent, remove_stopwords=True, remove_punc=True, mode='spacy'):
     """
     basic tokenizer method from spacy
     :param sent: input sentence
     :param remove_stopwords: whether to remove stopwords
     :param remove_punc: whether to remove punctuation
+    :param mode: {'spacy', 'naive'}
     :return: list of tokens
     """
-    tokens = tokenizer(sent)
-    if remove_punc:
-        tokens = [token.text.lower() for token in tokens if (token.text not in punctuations)]
+    if mode == 'spacy':
+        tokens = tokenizer(sent)
+        tokens = [token.text for token in tokens]
+    elif mode == 'naive':
+        tokens = sent.split(" ")
+    else:
+        tokens = []
 
-    if remove_stopwords:
+    if remove_stopwords:  # only removed if small cap
         tokens = [token for token in tokens if token not in stop_words.ENGLISH_STOP_WORDS]
 
+    if remove_punc:
+        tokens = [token.lower() for token in tokens if (token not in punctuations)]
+    else:
+        tokens = [token.lower() for token in tokens]
+
+    # returns lower case, scrubbed tokens
     return tokens
 
 
-def extract_ngram_from_text(text, n, remove_stopwords=True, remove_punc=True):
+def extract_ngram_from_text(text, n, remove_stopwords=True, remove_punc=True, mode='spacy'):
     """
     Function that retrieves all n-grams from the input string
-    @param text: raw string
-    @param n: integer that tells the model to retrieve all k-gram where k<=n
-    @param remove_stopwords: whether or not to remove stopwords from lib
-    @param remove_punc: whether or not to remove punctuation from lib
-    @return ngram_counter: a counter that maps n-gram to its frequency
-    @return tokens: a list of parsed ngrams
+    :param text: raw string
+    :param n: integer that tells the model to retrieve all k-gram where k<=n
+    :param remove_stopwords: whether or not to remove stopwords from lib
+    :param remove_punc: whether or not to remove punctuation from lib
+    :param mode: {'spacy', 'naive'}
+    :return ngram_counter: a counter that maps n-gram to its frequency
+    :return tokens: a list of parsed ngrams
     """
-    # tokenize words - for simplicity just split by space
-    tokens = tokenize(text, remove_stopwords=remove_stopwords, remove_punc=remove_punc)
-    # print("all tokens", str(tokens))
-
+    tokens = tokenize(text, remove_stopwords=remove_stopwords, remove_punc=remove_punc, mode=mode)
     all_ngrams = []
     for i in range(0, len(tokens) - n):
         for j in range(1, n + 1):
@@ -56,21 +69,23 @@ def construct_ngram_indexer(ngram_counter_list, topk):
     Function that selects the most common topk ngrams
     index 0 reserved for <pad>
     index 1 reserved for <unk>
-    @param ngram_counter_list: list of counters
-    @param topk, int: # of words to keep in the vocabulary - not counting pad/unk
-    @return ngram2idx: a dictionary that maps ngram to an unique index
+    :param ngram_counter_list: list of counters
+    :param topk: int, # of words to keep in the vocabulary - not counting pad/unk
+    :return ngram2idx: a dictionary that maps ngram to an unique index
     """
     rt_dict = {PAD_TOKEN: PAD_IDX, UNK_TOKEN: UNK_IDX}
     i = 2  # the index to start the rest of the tokens
     final_count = Counter()
-    for ngc in ngram_counter_list:
-        final_count += ngc
 
-    for key in dict(final_count.most_common(topk)):
+    for elem in tqdm(ngram_counter_list):
+        for key, value in elem.items():
+            final_count[key] += value
+
+    for key in tqdm(dict(final_count.most_common(topk))):
         rt_dict[key] = i
         i += 1
 
-    return rt_dict  # length topk + 2
+    return rt_dict, final_count  # length topk + 2
 
 
 def get_n_gram_at_position_i(n, i, tokens):
@@ -95,33 +110,48 @@ def token_to_index(tokens, ngram_indexer):
     Function that transform a list of tokens to a list of token index.
     index 0 reserved for <pad>
     index 1 reserved for <unk>
-    @param tokens: list of ngram
-    @param ngram_indexer: a dictionary that maps ngram to an unique index
+    :param tokens: list of ngram
+    :param ngram_indexer: a dictionary that maps ngram to an unique index
     """
     return [ngram_indexer[token] if token in ngram_indexer else UNK_IDX for token in tokens]
 
 
-def process_text_dataset(dataset, n, topk=None, ngram_indexer=None):
+def process_text_dataset(dataset,
+                         n,
+                         topk=None,
+                         ngram_indexer=None,
+                         remove_stopwords=True,
+                         remove_punc=True,
+                         mode='spacy'):
     """
     Top level function that encodes each datum into a list of ngram indices
-    @param dataset: list of IMDBDatum
-    @param n: n in "n-gram"
-    @param topk: #
-    @param ngram_indexer: a dictionary that maps ngram to an unique index
+    :param dataset: list of IMDBDatum
+    :param n: n in "n-gram"
+    :param topk: #
+    :param ngram_indexer: a dictionary that maps ngram to an unique index
+    :param remove_stopwords: bool to remove stopwords in the tokenizer
+    :param remove_punc: bool to remove punctuation in the tokenizer
+    :param mode: {'spacy', 'naive'}
     """
+    ngram_counter = None
     # extract n-gram
-    for i in tqdm(range(len(dataset))):
+    print("extracting ngrams ...")
+    for i in tnrange(len(dataset), desc='extract ngrams'):
         text_datum = dataset[i].raw_text
-        ngrams, tokens = extract_ngram_from_text(text_datum, n)
+        ngrams, tokens = extract_ngram_from_text(text_datum, n, remove_stopwords, remove_punc, mode)
         dataset[i].set_ngram(ngrams)
         dataset[i].set_tokens(tokens)
     # select top k ngram
     if ngram_indexer is None:
-        ngram_indexer = construct_ngram_indexer([datum.ngram for datum in dataset], topk)
+        print("constructing ngram_indexer ...")
+        ngram_indexer, ngram_counter = construct_ngram_indexer([datum.ngram for datum in dataset], topk)
+    else:
+        print("already have a passed ngram_indexer ...")
     # vectorize each datum
-    for i in range(len(dataset)):
+    print("setting each dataset's token indexes")
+    for i in tnrange(len(dataset), desc='token to index'):
         dataset[i].set_token_idx(token_to_index(dataset[i].tokens, ngram_indexer))
-    return dataset, ngram_indexer
+    return dataset, ngram_indexer, ngram_counter
 
 
 if __name__ == "__main__":
@@ -129,18 +159,3 @@ if __name__ == "__main__":
     print("testing ngrams module")
 
     my_sent = r'this apple is as red as they can be, said Sally. Then Sally left and became a silly Sally.'
-    print(extract_ngram_from_text(my_sent
-                                  , 2
-                                  , remove_stopwords=True
-                                  , remove_punc=True), "\n")
-
-    print(extract_ngram_from_text(my_sent
-                                  , 2
-                                  , remove_stopwords=False
-                                  , remove_punc=True), "\n")
-    print(extract_ngram_from_text(my_sent
-                                  , 3
-                                  , remove_stopwords=False
-                                  , remove_punc=False), "\n")
-
-
