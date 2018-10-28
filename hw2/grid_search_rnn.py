b17c70f951d249593bfa2d443d0cb27424a2b989
Rong Feng <rmfeng@gmail.com>
2018-10-28 19:34:24 -0400

    final commit

--- hw2/grid_search_rnn.py
+++ hw2/grid_search_rnn.py
@@ -11,36 +11,34 @@ import logging
 conf.init_logger(logging.INFO, logfile=None)
 
 mgr = mm.ModelManager()
-mgr.load_data(mm.loaderRegister.SNLI)
+
 
 # grid search params
-lr_list = [0.01, 0.001]
-decay_rates = [0.95, 0.8]
-rnn_hidden_sizes = [50, 100, 200]
-dropouts = [0.25, 0.5, 0.75]
+voc_sizes = [100000, 400000]
+rnn_hidden_sizes = [100, 400, 1600]
+dropouts = [0.2, 0.5]
 
 
 def hparam_to_label(tp):
-    label = 'mod'
-    label += ' lrinv' + str(int(1 / tp[0]))
-    label += ' decay' + str(int(tp[1] * 100))
-    label += ' rhs' + str(int(tp[2]))
-    label += ' drop' + str(int(tp[3] * 100))
+    label = 'rnn'
+    label += ' voc' + str(int(tp[0]))
+    label += ' rhs' + str(int(tp[1]))
+    label += ' drop' + str(int(tp[2] * 100))
     return label
 
 
-for hp_tuple in list(itertools.product(lr_list, decay_rates, rnn_hidden_sizes, dropouts)):
-    lr, decay, rnn_hidden, dropout = tuple(hp_tuple)
-    hparam_overrides = {HyperParamKey.LR: lr,
-                        HyperParamKey.SCHEDULER_GAMMA: decay,
+for hp_tuple in list(itertools.product(voc_sizes, rnn_hidden_sizes, dropouts)):
+    voc_size, rnn_hidden, dropout = tuple(hp_tuple)
+    hparam_overrides = {HyperParamKey.VOC_SIZE: voc_size,
                         HyperParamKey.RNN_HIDDEN_SIZE: rnn_hidden,
                         HyperParamKey.DROPOUT_FC: dropout,
                         HyperParamKey.DROPOUT_RNN: dropout}
 
     mgr.hparams.update(hparam_overrides)
+    mgr.load_data(mm.loaderRegister.SNLI)
     mgr.new_model(mm.modelRegister.NLIRNN, label=hparam_to_label(hp_tuple))
     mgr.train()
     mgr.graph_training_curves()
     mgr.dump_model()
 
-mgr.get_results().to_csv('model_saves/rnn_results.csv')
+mgr.get_results().to_csv('model_saves/rnn_rerun_results.csv')
